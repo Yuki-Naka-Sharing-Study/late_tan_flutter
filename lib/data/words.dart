fa3b037a63bcd37fa44cb4a5db58330f518d0f4b
仲優樹 <yu-ki216m@outlook.jp>
2025-04-03 19:43:56 +0900

    Create words list.

---
+++ lib/data/words.dart
@@ -0,0 +1,12 @@
+final List<Map<String, String>> words = [
+  {"es-ES": "ordenador", "es-LA": "computadora"},
+  {"es-ES": "coche", "es-LA": "carro"},
+  {"es-ES": "zumo", "es-LA": "jugo"},
+  {"es-ES": "gafas", "es-LA": "lentes"},
+  {"es-ES": "móvil", "es-LA": "celular"},
+  {"es-ES": "patata", "es-LA": "papa"},
+  {"es-ES": "conducir", "es-LA": "manejar"},
+  {"es-ES": "aseos", "es-LA": "baños"},
+  {"es-ES": "habitación", "es-LA": "cuarto"},
+  {"es-ES": "billete", "es-LA": "boleto"},
+];
